ac58539227b5ed1ce8339dc7b503b8e2d4fd1000
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-03-05 19:14:08 -0600

    vibe coding moment

--- setup.py
+++ setup.py
@@ -12,21 +12,25 @@
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 import os
-os.path.dirname(os.path.abspath(__file__))
+
+# Define the absolute path to the current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+glm_path = os.path.join(current_dir, "third_party/glm/")
 
 setup(
     ext_modules=[
         CUDAExtension(
             name="diff_gaussian_rasterization._C",
             sources=[
-            "cuda_rasterizer/rasterizer_impl.cu",
-            "cuda_rasterizer/forward.cu",
-            "cuda_rasterizer/backward.cu",
-            "rasterize_points.cu",
-            "ext.cpp"],
-            # extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
+                "cuda_rasterizer/rasterizer_impl.cu",
+                "cuda_rasterizer/forward.cu",
+                "cuda_rasterizer/backward.cu",
+                "rasterize_points.cu",
+                "ext.cpp"
+            ],
             extra_compile_args={"nvcc": [f"-I{glm_path}"]}
-        ],
+        )
+    ],
     cmdclass={
         'build_ext': BuildExtension
     }
