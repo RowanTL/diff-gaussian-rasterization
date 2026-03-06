7102e252bb4470ac2a2278754649b02a4d99c77a
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-03-05 19:12:42 -0600

    attempt to swtich to pyproject.toml

--- setup.py
+++ setup.py
@@ -15,8 +15,6 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
     ext_modules=[
         CUDAExtension(
             name="diff_gaussian_rasterization._C",
@@ -26,7 +24,8 @@ setup(
             "cuda_rasterizer/backward.cu",
             "rasterize_points.cu",
             "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
+            # extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
+            extra_compile_args={"nvcc": [f"-I{glm_path}"]}
         ],
     cmdclass={
         'build_ext': BuildExtension
